b1948c49f696cf2da7f4d26241ce0562b7b8e06b
Anna Marlena <annamarlena310@gmail.com>
2023-04-10 18:59:55 -0500

    Tried some more code. struggling with the local storage piece

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# workday-scheduler
+# Workday Scheduler
+
+## Objective
+
+The objective was to create a day planner page that updates with the correct day and date at the top of the page, and keeps track of the current hour in hour blocks for a regular workday. Past hours are gray, the current hour is red, and future hour blocks are green. When the user enters text into one of the hour blocks and clicks save, the text is saved. 
+
+## What I Learned
+
+I learned a lot about using jQuery and also dynamically generating the divs for the hour blocks. This did cause me trouble later on when I wanted to create the event listener. I'm still struggling to grasp how to capture local storage. 
+
+## Link to Published Repo
+
+## Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,7 @@
 // set the global variables
 var todaysDate = document.getElementById("currentDay");
-var taskDescription = $(".description");
-
-function getClassName(milTime){
-  var now = parseInt( dayjs().format("H") );
-  if( now > milTime ) {
-    return "past"
-  } else if ( now < milTime ){
-    return "future"
-  } else {
-    return "present"
-  }
-}
-
-function militaryToStandardTime(milTime){
-  if(milTime < 12) {
-    return milTime + "AM";
-  } else if (milTime === 12) {
-    return milTime + "PM";
-  } else {
-    return (milTime - 12) + "PM";
-  }
- 
-}
-
-for( var i = 9; i<= 17; i++ ){
-  var div = `<div class='row'>
-  <div class='col-2 col-md-1 hour text-center py-3'>${militaryToStandardTime(i)}</div>
-  <textarea class='col-8 col-md-10 description ${getClassName(i)}' rows='3'></textarea>
-  <button class='btn saveBtn col-2 col-md-1' aria-label='save'>
-    <i class='fas fa-save' aria-hidden='true'></i>
-  </button>
-</div>`
-  $( ".container-lg" ).append(div)
-}
-
+var taskDescription = document.querySelector(".description");
+var saveBtn = document.querySelector(".saveBtn");
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -53,15 +20,12 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $(document).ready(function(){
-    $(".btn").click(function(){
-      console.log("click")
-    })
-  })
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
+  // $(document).ready(function(){
+  //   $(".btn").click(function(){
+  //     console.log("click")
+  //   })
+  // })
+   
   // Get the current hour in 24-hour time
   function currentHour() {
     now = dayjs().format("H");
@@ -69,23 +33,67 @@ $(function () {
   }
   currentHour()
 
- 
-  //
+  // Apply the past, present, or future class to each div based on current time
+  function getClassName(milTime){
+    var now = parseInt( dayjs().format("H") );
+    if( now > milTime ) {
+      return "past"
+    } else if ( now < milTime ){
+      return "future"
+    } else {
+      return "present"
+    }
+  }
+
+  // Add the time in 12-hour time to column 1 of each row
+  function militaryToStandardTime(milTime){
+    if(milTime < 12) {
+      return milTime + "AM";
+    } else if (milTime === 12) {
+      return milTime + "PM";
+    } else {
+      return (milTime - 12) + "PM";
+    }
+  }
+
+  // Create the div rows and columns dynamically
+  for( var i = 9; i<= 17; i++ ){
+    var div = `<div class='row'>
+    <div class='col-2 col-md-1 hour text-center py-3'>${militaryToStandardTime(i)}</div>
+    <textarea class='col-8 col-md-10 description ${getClassName(i)}' rows='3'></textarea>
+    <button class='btn saveBtn col-2 col-md-1' aria-label='save'>
+      <i class='fas fa-save' aria-hidden='true'></i>
+    </button>
+  </div>`
+    $( ".container-lg" ).append(div)
+  }
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  // $("button").on("click", function(event) {
+  //   console.log("new click")
+  //   event.preventDefault();
+  //   document.getElementsByClassName("description").textContent = taskDescription.value;
+  //   console.log("Saved text: " + taskDescription)
+  // });
+  
+  // Create a submit event listener on the form element
+  //Adds an event listener for the "button"
+// $("button").on("click", function (event) { 
+// event.preventDefault();
+// console.log("hello")
+// //Sets item to the value of the text area for the appropriate time block 
+//   for ( i = 0; i < 12; i++) {
+//       localStorage.setItem('textarea' + String(i + 1), $("textarea")[i].value) 
+//   }
+// });
   
 });
 
 
 
-//   // select form element by its `name` attribute and get its value
-//   taskDescription = $('input[name="task"]').val();
-//   console.log(taskDescription.val())
 
-// }
-// // Create a submit event listener on the form element
-// taskDescription.on('submit', handleFormSubmit);
 
 
 
